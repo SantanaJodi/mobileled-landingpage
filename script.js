65593242d86923860a492e0cd063aa74eab1659d
SantanaJodi <targetsan21@gmail.com>
2019-12-09 03:03:51 +0700

    back to normal (no location.reload())

--- script.js
+++ script.js
@@ -124,7 +124,7 @@ $(document).ready(function(){
     });
   }
 
-  $(window).resize(function(){
-    location.reload();
-  })
+  // $(window).resize(function(){
+  //   location.reload()
+  // })
 })
